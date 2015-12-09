b5db866c6c4cd8b589bb7dfb6403f5ad0ba65379
suchita <suchita@yahoo-inc.com>
2015-12-09 01:21:19 -0800

    papa-parser v1

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-papaparser'
+  name: 'papaparser',
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+
+    app.import(app.bowerDirectory + '/papaparse/papaparse.js');
+  }
 };
