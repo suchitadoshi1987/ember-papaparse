607a721eea6436c181a2c22075ec87b41219ddcd
suchita <suchita@yahoo-inc.com>
2015-12-09 01:46:55 -0800

    Initial version

---
+++ blueprints/ember-papaparse/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  description: 'Installation blueprint for ember-papaparse-shim',
+
+  normalizeEntityName: function() {},
+
+  afterInstall: function(options) {
+    return this.addBowerPackageToProject('papaparse', '~4.1.2'));
+  }
+};
